d28c0f5243e00f1e499b718180391de3d01faf81
Shikhar <devilshikhar123@gmail.com>
2025-07-02 16:53:35 +0530

    Temp rename for case correction

--- src/app/recruitment/page.jsx
+++ src/app/recruitment/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import CandidateManagement from './CandidateManagement/page'
+import CandidateManagement from './candidatemanagement/page'
 import InterviewScheduling from './InterviewScheduling/page'
 import JobPostings from './JobPostings/page'
 import OnboardingProcess from './OnboardingProcess/page'
